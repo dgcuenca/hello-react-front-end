157dc2df276ba363394b42bf480b185b3c925a25
diego <dgcuenca@gmail.com>
2023-05-23 22:44:05 -0500

    Set up storage and reducer

--- src/index.js
+++ src/index.js
@@ -2,13 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import setupStore from './redux/configureStore';
+import { apiSlice } from './redux/apis/apiSlice';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const store = setupStore({});
+
+store.dispatch(apiSlice.endpoints.getMessage.initiate());
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
